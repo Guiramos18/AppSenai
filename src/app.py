9bec2e8e20d1feb6b9a4655ffa69ea303a36acd9
Guiramos18 <vnicodemoramos@gmail.com>
2026-03-24 13:30:08 -0300

    .

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import flet
-from flet import ThemeMode, Text, TextField, OutlinedButton, Column, CrossAxisAlignment
+from flet import ThemeMode, Text, TextField, OutlinedButton, Column, CrossAxisAlignment, ElevatedButton, TextButton, \
+    Container, Colors
+from datetime import datetime
 from flet.controls import page
 from flet.controls.border_radius import horizontal
 
@@ -30,48 +32,90 @@ def main(page: flet.Page):
             texto = 'Impar'
 
 
-        text.value = f"Seu numero é {texto}"
+        text2.value = f"Seu numero é {texto}"
         page.update()
 
     def verificar_idade():
         idade_convertida = int(data_nasc.value)
-        sua_idade= 2026 - idade_convertida
+        sua_idade= datetime.now().year - idade_convertida
         if sua_idade >= 18:
             texto = 'maior de idade'
 
         else:
             texto = 'menor de idade'
 
-        text.value = f"Você é {texto}"
+        text3.value = f"Você é {texto}"
         page.update()
 
 
 
 
     # Componentes
-    text = Text("Ola mundo")
+    text = Text("")
+    text2 = Text("")
+    text3 = Text("")
 
     input_nome = TextField(label="Nome")
     input_nome2 = TextField(label="Sobrenome")
     numero1 = TextField(label="Digite o numero")
-    data_nasc = TextField(label="Data de nascimento")
+    data_nasc = TextField(label="Ano de nascimento")
     btn_salvar = OutlinedButton("Salvar", on_click=salvar_nome)
-    btn_impar_par = OutlinedButton("Impar ou par", on_click=impar_par)
-    btn_idade = OutlinedButton("Verificar idade", on_click=verificar_idade)
+    btn_impar_par = ElevatedButton("Impar ou par", on_click=impar_par)
+    btn_idade = TextButton("Verificar idade", on_click=verificar_idade)
 
 
     # Construção da tela
     page.add(
         Column(
             [
-            input_nome,
-            input_nome2,
-            numero1,
-            data_nasc,
-            btn_salvar,
-            btn_impar_par,
-            btn_idade,
-            text,
+                Container(
+                    Column(
+                        [
+                            Text("Atividade 1", weight="bold"),
+                            input_nome,
+                            input_nome2,
+                            btn_salvar,
+                            text,
+                        ],
+                        horizontal_alignment=CrossAxisAlignment.CENTER,
+                    ),
+                    bgcolor=Colors.BLUE_500,
+                    padding=15,
+                    border_radius=10,
+                    width=400,
+                ),
+
+                Container(
+                    Column(
+                        [
+                            Text("Atividade 2", weight="bold"),
+                            numero1,
+                            btn_impar_par,
+                            text2,
+                        ],
+                        horizontal_alignment=CrossAxisAlignment.CENTER,
+                    ),
+                    bgcolor=Colors.AMBER_600,
+                    padding=15,
+                    border_radius=10,
+                    width=400,
+                ),
+
+                Container(
+                    Column(
+                        [
+                            Text("Atividade 3", weight="bold"),
+                            data_nasc,
+                            btn_idade,
+                            text3,
+                        ],
+                        horizontal_alignment=CrossAxisAlignment.CENTER,
+                    ),
+                    bgcolor=Colors.GREEN_800,
+                    padding=15,
+                    border_radius=10,
+                    width=400,
+                ),
             ],
             width=400,
             horizontal_alignment=CrossAxisAlignment.CENTER
@@ -80,4 +124,4 @@ def main(page: flet.Page):
 
 
 
-flet.app(main)
+flet.run(main)
